09995e98556e1e63a3b93c135d67c3f75b035d6e
fin <49364048+FinChalkley@users.noreply.github.com>
2025-09-22 14:05:44 +0100

    added animated background

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-VANTA.CLOUDS({
-  el: "#vantajs",
-   mouseControls: true,
-  touchControls: true,
-  gyroControls: false,
-  minHeight: 200.00,
-  minWidth: 200.00,
-  skyColor: 0x1488cc,
-  cloudColor: 0x6dd5fa
+window.addEventListener("DOMContentLoaded", () => {
+  VANTA.CLOUDS({
+    el: "#vantajs",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    skyColor: 0x1488cc,
+    cloudColor: 0x6dd5fa
+  });
 });
